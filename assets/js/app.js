21ca648952e86383ce64e66f5d19336015d2f8f9
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-14 16:28:22 +0200

    Borrar Tweet del DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,10 +27,38 @@ function agregarTweet(event){
     listaTweets.appendChild(li);
 }
 
+//Eliminar Tweet del listado
+function borrarTweet(event){
+    /*
+    Esta función se dispara cuando pulso en cualquier elemento de la lista.
+    Para eso se usa Delegation, para saber exactamente en cual de todos los 
+    elementos de la lista se ha pulsado.
+
+    .classList => Regresa un arreglo con todas las class asociadas al elemento
+    .className => Regresa todas las clases que haya (ideal si solo hay una)
+    */
+
+    //console.log('Diste click en la lista');
+    event.preventDefault();
+
+    //Delegation
+    if( event.target.className === 'borrar-tweet' ){
+        //console.log('Diste click en eliminar');
+        //Elimino el padre del botón (li)
+        event.target.parentElement.remove();
+        alert('Tweet Eliminado');
+    }else{
+        //console.log('Diste click en otra parte');
+    }
+}
+
 // Event Listeners
 function eventListeners(){
     //Listener del formulario cuando se envia.
     document.querySelector('#formulario').addEventListener('submit', agregarTweet);
+
+    //Lister de la lista de Tweets
+    listaTweets.addEventListener('click', borrarTweet);
 }
 
 eventListeners();
